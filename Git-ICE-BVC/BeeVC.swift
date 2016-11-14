357ecbe0c125602a39145067e376b89c1afb1087
Student <s525704@ch140025m.nwmissouri.edu>
2016-11-14 16:46:50 -0600

    Added the Third VC

--- Git-ICE-BVC/BeeVC.swift
+++ Git-ICE-BVC/BeeVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  Git-ICE-BVC
 //
 //  Created by Bobba,Vamsy Chowdary on 11/14/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class BeeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
