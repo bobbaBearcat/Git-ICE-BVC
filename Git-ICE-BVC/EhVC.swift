f922e5028d4f247c1ec54fad377df6f6b000f56c
Student <s525704@nw36085.nwmissouri.edu>
2016-11-16 16:17:55 -0600

    Edited Eh

--- Git-ICE-BVC/EhVC.swift
+++ Git-ICE-BVC/EhVC.swift
@@ -17,9 +17,11 @@ class EhVC: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 
 
+    
 }
 
