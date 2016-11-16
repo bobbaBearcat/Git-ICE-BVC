028144e82a399a477800cc15ea3ab817e956832b
Student <s525741@ch140024m.nwmissouri.edu>
2016-11-16 16:26:38 -0600

    ME

--- Git-ICE-BVC/BeeVC.swift
+++ Git-ICE-BVC/BeeVC.swift
@@ -13,7 +13,7 @@ class BeeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Help-B")
+        print("Help-ME")
     }
 
     @IBAction func handleClick(sender: AnyObject){
